c1d4efd7ad76a750315893c57066caa332c3023a
Akrati <singhakrati0101@gmail.com>
2023-11-13 13:12:10 +0530

    basic UI design

--- public/script.js
+++ public/script.js
@@ -5,3 +5,19 @@ const btnNavEl = document.querySelector(".btn-mobile-nav");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+
+// scroll to the top
+
+const scrollUpbtn = document.querySelector("#scroll-up");
+
+function scrollUp(){
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",})
+
+}
+
+scrollUpbtn.addEventListener("click",scrollUp)
+
