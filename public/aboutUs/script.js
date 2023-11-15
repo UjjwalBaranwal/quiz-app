a2f900a96faafc4b4586afb4dd295ba3ce2c0b68
Akrati <singhakrati0101@gmail.com>
2023-11-15 12:28:30 +0530

    added about us page

---
+++ public/aboutUs/script.js
@@ -0,0 +1,21 @@
+
+
+
+// SWIPPER CODE
+// var swiper = new Swiper(".card-container", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   slidesPerGroup: 3,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".fa-solid fa-angle-right",
+//     prevEl: ".fa-solid fa-angle-left",
+//   },
+// });
+
+
