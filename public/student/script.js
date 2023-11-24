bd9d1c5d6c28de079cfb72904691dea4a764b878
VarnikaGoel2004 <vanu2004goel@gmail.com>
2023-11-25 01:23:21 +0530

    teacher section has been added

---
+++ public/student/script.js
@@ -0,0 +1,27 @@
+function menuToggle(){
+  const toggleMenu = document.querySelector('.menu');
+  toggleMenu.classList.toggle('active')
+} 
+
+// student dashboard
+ 
+const allIndicator = document.querySelectorAll('.indicator li');
+const allContent = document.querySelectorAll('.tab-content li');
+
+allIndicator.forEach(item=> {
+  item.addEventListener('click', function () {
+    const content = document.querySelector(this.dataset.target);
+
+    allIndicator.forEach(i=> {
+      i.classList.remove('active');
+    })
+
+    allContent.forEach(i=> {
+      i.classList.remove('active');
+    })
+
+    content.classList.add('active');
+    this.classList.add('active');
+  })
+})
+
