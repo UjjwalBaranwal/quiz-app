168dabce2f188daed837e339156e50e788d25dac
Akrati <singhakrati0101@gmail.com>
2023-11-25 11:38:16 +0530

    added quiz section

--- public/create-quiz.js
+++ public/create-quiz.js
@@ -1,12 +1,12 @@
 const btn = document.getElementById('add');
-const div = document.getElementsByClassName('create');
-btn.addEventListener('click', addquestion());
+const div = document.querySelectorAll('.temp-container');
 
-function addquestion(){
-    div.innerHTML+=`
+document.getElementById('add').addEventListener('click', function(){
+
+    document.querySelectorAll('.temp-container').append.innerHTML = `
         <input type="text" placeholder="question 1">Enter the question</input>
         <input type="text" placeholder="option 1">Enter the option 1</input>
         <input type="text" placeholder="option 2">Enter the option 2</input>
         <input type="text" placeholder="option 3">Enter the option 3</input>
         <input type="text" placeholder="option 4">Enter the option 4</input>`
-}
+});
